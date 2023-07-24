1836572307744920eb5660ebd2da8d0c2af01fd5
Antonton154 <104890337+Antonton154@users.noreply.github.com>
2023-07-24 14:10:00 -0400

    update

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 for (let planet of document.getElementsByClassName("planets")) {
     planet.addEventListener("click", changeWardrobe, false)
 }
-for (let clothe of document.get)
+for (let clothe of document.getElementsByClassName("clothes")) {
+    clothe.addEventListener("click", changeClothe, false)
+}
 
 function changeWardrobe() {
     let name = this.id
@@ -14,4 +16,8 @@ function changeWardrobe() {
         wardrobe.style.setProperty("display", "flex")
         console.log("set flex")
     }
+}
+
+function changeClothe() {
+    
 }
